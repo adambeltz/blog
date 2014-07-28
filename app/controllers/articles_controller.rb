8511aebf59e9e93c9330c246131998c114c1dff1
Adam Beltz <adambeltz@hotmail.com>
2014-07-28 09:53:47 -0400

    sure

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,25 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
-    
+    @article = Article.new(article_params)
+ 
     @article.save
     redirect_to @article
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def index
+    @articles = Article.all
   end
 
    private
     def article_params
       params.require(:article).permit(:title, :text)
     end
-
-
-
-  def show
-    @article = Article.find(params[:id])
-  end
 end
+
+
+
